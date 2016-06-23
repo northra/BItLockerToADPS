ec25b55ce081960030514942cb77568798512c00
stephenmartin <sudormminusrf@gmail.com>
2016-06-23 08:39:39 +0100

    removed links from script now in readme.

--- BitlockerToAdPS.ps1
+++ BitlockerToAdPS.ps1
@@ -1,11 +1,5 @@
 ﻿# Backup BitLocker recovery keys to Active Directory
 # Stephen Martin <sudormminusrf@gmail.com>
-#
-# Windows 8 and above, Powershell 4 and above required.
-# You are required to have local admin rights to be able to use this script.
-
-# Correct group policy to allow saving keys to ADDS http://windowsitpro.com/windows-8/group-policy-settings-bitlocker
-# Add BitLocker tab to ADUC http://www.alexandreviot.net/2015/06/10/active-directory-how-to-display-bitlocker-recovery-key/
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
